94f465414d999e64e37d12b955fe24fec0cbc3c2
Saitama <gourabnag12@gmail.com>
2018-09-14 10:35:52 +0530

    support for different contexes.

--- Source/JavaScript/Components/ChatApp/ChatApp.jsx
+++ Source/JavaScript/Components/ChatApp/ChatApp.jsx
@@ -5,16 +5,45 @@ import "../../../Styles/Chat.scss";
 
 export default class ChatApp extends Component {
   state = {
-    activeChat: null
+    activeChat: [
+      null,
+      null,
+      null
+    ]
   };
 
-  setActiveChat = chatContext => this.setState({ activeChat: chatContext });
+  setActiveChat = chatContext => {
+    this.setState(prevState => {
+      const activeChat = [...prevState.activeChat];
+
+      // Fist check if the specified context is already open, if yes, short circuit.
+      if(prevState.activeChat.includes(chatContext)) return prevState;
+
+      if(prevState.activeChat[0] === null) {
+        activeChat[0] = chatContext;
+      } else if (prevState.activeChat[1] === null) {
+        activeChat[1] = chatContext;
+      } else if (prevState.activeChat[2] === null) {
+        activeChat[2] = chatContext;
+      } else {
+        // If everything is open, just open it in the last window
+
+        activeChat[2] = chatContext;
+      }
+
+      return {
+        activeChat
+      };
+    });
+  };
 
   render() {
     return (
       <React.Fragment>
         <ChatPeopleSidebar websocket={this.props.websocket} onChange={this.setActiveChat} />
-        <ChatContent chatContext={this.state.activeChat} websocket={this.props.websocket} activeUsername={this.props.activeUsername} />
+        {this.state.activeChat.map((chat, index) => (
+          <ChatContent chatContext={chat} websocket={this.props.websocket} activeUsername={this.props.activeUsername} key={index} />
+        ))}
       </React.Fragment>
     )
   }
